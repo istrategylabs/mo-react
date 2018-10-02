626ddf3c3fc572d3ad7e8190de3b3c11a42e60c8
Scott Donaldson <scott@isl.co>
2018-10-02 14:03:27 -0400

    use react-snapshot

--- {{cookiecutter.repo_name}}/src/App.js
+++ {{cookiecutter.repo_name}}/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet title="{{ cookiecutter.project_name }}" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
