a3453acde82cf1d1f6f4407ba56bdac72cf26b31
TiffanieISL <tiffanie@isl.co>
2018-11-05 10:31:41 -0500

    updates to correct meta -> props

--- {{cookiecutter.repo_name}}/src/components/Meta.js
+++ {{cookiecutter.repo_name}}/src/components/Meta.js
@@ -6,13 +6,12 @@ const Meta = (props) => {
     {props.title ? <title>{props.title}</title> : null}
     {props.description ? <meta property="og:description" content={props.description} /> : null}
     {props.description ? <meta name="description" content={props.description} /> : null}
-    {props.site_name ? <meta name="twitter:site" content={meta.twitter_profile} /> : null}
-    {props.creator ? <meta name="twitter:creator" content={meta.creator} /> : null}
-    {props.img ? <meta property="og:image" content={meta.image} /> : null}
-    {props.url ? <meta property="og:url" content={meta} /> : null}
-    {props.type ? <meta property="og:type" content={meta.type} /> : null}
-    {props.twitter_profile ? <meta property="og:type" content={meta.type} /> : null}
-
+    {props.site_name ? <meta name="twitter:site" content={props.site_name} /> : null}
+    {props.creator ? <meta name="twitter:creator" content={props.creator} /> : null}
+    {props.img ? <meta property="og:image" content={props.image} /> : null}
+    {props.url ? <meta property="og:url" content={props.url} /> : null}
+    {props.type ? <meta property="og:type" content={props.type} /> : null}
+    {props.twitter_profile ? <meta property="og:type" content={props.twitter_profile} /> : null}
   </Helmet>
 }
 
