5616565b699b6e01405156c7e84ed65eda5fd887
TiffanieISL <tiffanie@isl.co>
2018-11-05 09:39:55 -0500

    removes extra <, adds detail to meta object

--- {{cookiecutter.repo_name}}/src/App.js
+++ {{cookiecutter.repo_name}}/src/App.js
@@ -16,29 +16,19 @@ const title = '{{ cookiecutter.project_name }}';
 class App extends Component {
   render() {
     const meta = {
-      description: "This is the description of your website",
+      description: '{{cooliecutter.description}}',
       site_name: title,
-      type:"This is the type of your website",
-      image:"" ,
-      url:"" ,
-      twitter_profile: "",
-      creator: ""
+      type:"PLEASE CHANGE THIS TO: website name",
+      image:"PLEASE CHANGE THIS TO: featured image",
+      url:"PLEASE CHANGE THIS TO: featured url" ,
+      twitter_profile: "PLEASE CHANGE THIS TO: featured profile",
+      creator: "PLEASE CHANGE THIS TO: creator of featured content"
     }
     return (
       <Wrapper>
         <Helmet>
           <title>{title}</title>
-          <meta property="og:title" content={title} />
-          <meta property="og:site_name" content={meta.site_name} />
-          <meta property="og:description" content={meta.description} />
-          <meta property="og:type" content={meta.type} />
-          <meta property="og:image" content={meta.image} />
-          <meta property="og:url" content={meta} />
-          <meta name="twitter:card" content={meta.description} />
-          <meta name="twitter:site" content={meta.twitter_profile} />
-          <meta name="twitter:creator" content={meta.creator} />
         </Helmet>
-        />
         <div>
           <Header />
           <Switch>
